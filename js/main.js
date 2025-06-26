126cced3def0c23f76c8e10a3ca6865af06cd4c0
unknown <adu19981392@gmail.com>
2025-06-26 20:13:27 +0200

    change style

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-const hamburger=document.querySelector(".hamburger"),nav=document.querySelector(".nav"),itemHome=document.querySelector(".homeItem"),itemAboutMe=document.querySelector(".aboutMeItem"),itemOfferts=document.querySelector(".offertsItem"),itemContact=document.querySelector(".contactItem"),itemFaq=document.querySelector(".faqItem"),accordionHeaders=document.querySelectorAll(".accordionHeader"),accordionContents=document.querySelectorAll(".accordionContent"),copyrightYear=document.querySelector(".copyrightYear").innerText=new Date().getFullYear(),showMenu=()=>{hamburger.classList.toggle("hamburgerActive"),nav.classList.toggle("navActive")};accordionHeaders.forEach(e=>{e.addEventListener("click",()=>{let t=e.parentElement.querySelector(".accordionContent");accordionContents.forEach(e=>{e!==t&&e.classList.remove("active")}),t.classList.toggle("active")})}),hamburger.addEventListener("click",showMenu),itemHome.addEventListener("click",showMenu),itemAboutMe.addEventListener("click",showMenu),itemOfferts.addEventListener("click",showMenu),itemContact.addEventListener("click",showMenu),itemFaq.addEventListener("click",showMenu);
+const hamburger = document.querySelector('.hamburger'),
+	nav = document.querySelector('.nav'),
+	itemHome = document.querySelector('.homeItem'),
+	itemAboutMe = document.querySelector('.aboutMeItem'),
+	itemOfferts = document.querySelector('.offertsItem'),
+	itemContact = document.querySelector('.contactItem'),
+	itemFaq = document.querySelector('.faqItem'),
+	accordionHeaders = document.querySelectorAll('.accordionHeader'),
+	accordionContents = document.querySelectorAll('.accordionContent'),
+	copyrightYear = (document.querySelector('.copyrightYear').innerText = new Date().getFullYear()),
+	showMenu = () => {
+		hamburger.classList.toggle('hamburgerActive'), nav.classList.toggle('navActive')
+	}
+accordionHeaders.forEach(e => {
+	e.addEventListener('click', () => {
+		let t = e.parentElement.querySelector('.accordionContent')
+		accordionContents.forEach(e => {
+			e !== t && e.classList.remove('active')
+		}),
+			t.classList.toggle('active')
+	})
+}),
+	hamburger.addEventListener('click', showMenu),
+	itemHome.addEventListener('click', showMenu),
+	itemAboutMe.addEventListener('click', showMenu),
+	itemOfferts.addEventListener('click', showMenu),
+	itemContact.addEventListener('click', showMenu),
+	itemFaq.addEventListener('click', showMenu)
